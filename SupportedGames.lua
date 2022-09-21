aecaf2976e2daca72dbd7f2694901e598b67d1d3
TheCircularCube <109701704+TheCircularCube@users.noreply.github.com>
2022-09-21 17:26:38 -0500

    Update SupportedGames.lua

--- SupportedGames.lua
+++ SupportedGames.lua
@@ -1,4 +1,4 @@
 gamelist = {
-[537413528], --Build A Boat For Treasure https://www.roblox.com/games/537413528/Build-A-Boat-For-Treasure
+[537413528] = true, -- Build A Boat For Treasure --> https://www.roblox.com/games/537413528/Build-A-Boat-For-Treasure
 }
 return gamelist
